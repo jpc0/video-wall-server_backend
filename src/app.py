4c35504e6dd61a821aecb75ba32ac68016a01863
Jean-Pierre Coetzee <jeanpierrec19@gmail.com>
2022-04-22 07:46:11 +0200

    Added more input checking and fixed API to deal with new caching

--- src/app.py
+++ src/app.py
@@ -1,7 +1,5 @@
-import time
 import os
-import json
-from urllib import response
+from dataclasses import dataclass
 from flask import Flask, Response, request, redirect, send_from_directory, jsonify
 from werkzeug.utils import secure_filename
 import zmq
@@ -14,20 +12,17 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 all_files_list = os.listdir(app.config['UPLOAD_FOLDER'])
 
 
-class image_file_data:
+@dataclass
+class ImageFileData:
     id: int
     file_path: str
-
-    def __init__(self, id: int, file_path: str) -> None:
-        self.id = id
-        self.file_path = file_path
 
     def __iter__(self):
         for key in self.__dict__:
             yield key, getattr(self, key)
 
 
-all_files: list[image_file_data] = []
+all_files: list[ImageFileData] = []
 
 
 def image_in_list(image: str) -> bool:
@@ -44,7 +39,7 @@ def update_all_files():
     for i in all_files_list:
         if image_in_list(i):
             continue
-        all_files.append(image_file_data(all_files_list.index(i)+1, i))
+        all_files.append(ImageFileData(all_files_list.index(i) + 1, i))
     for i in all_files:
         if not i:
             continue
@@ -72,106 +67,113 @@ def default_route():
 @app.route("/upload", methods=["POST"])
 def upload_route():
     if 'file' not in request.files:
-        response = jsonify({
+        res = jsonify({
             "version": "0.1.0",
             "message": "info",
             "content": [
                 "No file submitted"
             ]
         })
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
+        res.headers.add('Access-Control-Allow-Origin', '*')
+        return res
     file = request.files['file']
     # If the user does not select a file, the browser submits an
     # empty file without a filename.
     if file.filename == '':
-        response = jsonify({
+        res = jsonify({
             "version": "0.1.0",
             "message": "info",
             "content": [
                 "No file selected"
             ]
         })
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
+        res.headers.add('Access-Control-Allow-Origin', '*')
+        return res
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        response = jsonify({
+        res = jsonify({
             "version": "0.1.0",
             "message": "info",
             "content": [
                 "Success"
             ]
         })
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
+        res.headers.add('Access-Control-Allow-Origin', '*')
+        return res
 
-    response = jsonify({
+    res = jsonify({
         "version": "0.1.0",
         "message": "info",
         "content": [
             "Invalid file type submitted"
         ]
     })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
 
 
 @app.route("/get_all", methods=["GET"])
 def get_all_route():
     update_all_files()
     if all_files:
-        response = jsonify({
+        res = jsonify({
             "version": "0.1.0",
             "message": "all_files",
             "content": list(map(lambda i: dict(i), all_files))
         })
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
+        res.headers.add('Access-Control-Allow-Origin', '*')
+        return res
     else:
-        response = jsonify({
+        res = jsonify({
             "version": "0.1.0",
             "message": "info",
             "content": [
                 "No Files uploaded"
             ]
         })
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
+        res.headers.add('Access-Control-Allow-Origin', '*')
+        return res
 
 
 @app.route("/get/<filename>", methods=["GET"])
 def get_route(filename):
-    response = send_from_directory(
+    res = send_from_directory(
         app.config["UPLOAD_FOLDER"], filename)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
 
 
 @app.route("/display/<id>", methods=["GET"])
-def display_route(id):
+def display_route(id_):
+    if not filename_from_id(int(id_)):
+        res = jsonify({
+            "version": "0.1.0",
+            "message": "info",
+            "content": [
+                "No file selected"
+            ]
+        })
+        res.headers.add('Access-Control-Allow-Origin', '*')
+        return res
     context = zmq.Context()
     _requester = context.socket(zmq.REQ)
     _requester.connect("tcp://127.0.0.1:9091")
     data = {
         "version": "0.1.0",
         "command": "display_image",
-        "location": f"{UPLOAD_FOLDER}{filename_from_id(int(id))}",
+        "location": f"http://backend.woordenlewe.com/get/{filename_from_id(int(id_))}",
     }
     _requester.send_json(data)
-    response = jsonify(_requester.recv_json())
+    res = jsonify(_requester.recv_json())
     _requester.close()
     context.term()
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
 
 
 @app.route("/display_many", methods=["GET"])
 def display_many_route():
-    context = zmq.Context()
-    _requester = context.socket(zmq.REQ)
-    _requester.connect("tcp://127.0.0.1:9091")
     data = {
         "version": "0.1.0",
         "command": "display_image_loop",
@@ -179,14 +181,32 @@ def display_many_route():
         "locations": [],
     }
 
-    for id in request.args.get('ids').split(','):
-        data["locations"].append(f"{UPLOAD_FOLDER}{filename_from_id(int(id))}")
+    for i in request.args.get('ids').split(','):
+        if not filename_from_id(int(i)):
+            continue
+        data["locations"].append(f"http://backend.woordenlewe.com/get/{filename_from_id(int(i))}")
+
+    if not data["locations"]:
+        res = jsonify({
+            "version": "0.1.0",
+            "message": "info",
+            "content": [
+                "No files selected"
+            ]
+        })
+        res.headers.add('Access-Control-Allow-Origin', '*')
+        return res
+
+    context = zmq.Context()
+    _requester = context.socket(zmq.REQ)
+    _requester.connect("tcp://127.0.0.1:9091")
+
     _requester.send_json(data)
-    response = jsonify(_requester.recv_json())
+    res = jsonify(_requester.recv_json())
     _requester.close()
     context.term()
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
 
 
 @app.route("/adjust_color", methods=["GET"])
@@ -207,23 +227,23 @@ def adjust_color_route():
         "value": request.args.get("value", type=float),
     }
     _requester.send_json(data)
-    response = jsonify(_requester.recv_jsosn())
+    res = jsonify(_requester.recv_jsosn())
     _requester.close()
     context.term()
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
 
 
 @app.route("/delete/<id>", methods=["GET"])
-def delete_route(id):
+def delete_route(id_):
     os.remove(os.path.join(
-        app.config['UPLOAD_FOLDER'], filename_from_id(int(id))))
-    response = jsonify({
+        app.config['UPLOAD_FOLDER'], filename_from_id(int(id_))))
+    res = jsonify({
         "version": "0.1.0",
         "message": "info",
         "content": [
-            f"You deleted {id}"
+            f"You deleted {id_}"
         ]
     })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
